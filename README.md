bbcf3d5d05852174c9cc595c5bc719d6bd014d46
scauos <645670644@qq.com>
2018-05-10 21:52:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeanCloud-kit
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import LeanCloud_kitTests
+
+var tests = [XCTestCaseEntry]()
+tests += LeanCloud_kitTests.allTests()
+XCTMain(tests)
